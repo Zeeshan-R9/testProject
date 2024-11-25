2ab3e8b3289df03405bdff4cec7a32354fbf6baa
zoargamer5 <zoargamer5@gmail.com>
2024-11-25 17:37:51 +0500

    Initial commit

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-
-from Pexels import Client
-from pprint import pprint 
-client = Client(token="oAaoTX5Bnfsb58jacFxWsReS4v7luZiJYYLMlR5Gj3W6jqFWaoD3pCiC")
-
-#search photos
-photos = client.search_photos(query="Dog", page=1, per_page=80)
-
-
-#you can access return data like
-pprint(len(photos.photos))
+print('This is me writing python code')
