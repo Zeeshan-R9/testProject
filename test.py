00e5c3df96abc0c98362b17d0548e5dd1b2d8e60
zoargamer5 <zoargamer5@gmail.com>
2024-11-25 22:55:38 +0500

    Initinal commit

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-print('This is me writing python code')
+print('This is me writing python code2222')
 print('Added another line of code to test \"git diff \" command in terminal.')
+
+print('Hello! I will cause conflict!')
